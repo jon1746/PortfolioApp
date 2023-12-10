825fabc9b1e0a6ff3ce8478c2edde78eb326e7ea
Jon Waddell <jon.waddell@trexin.com>
2023-12-10 17:34:56 -0600

    Fixed stuff

--- app.js
+++ app.js
@@ -1,59 +1,32 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
 
-var indexRouter = require('./routes/index');
-var whatidoRouter = require('./routes/whatido');
-var galleryofmyworkRouter = require('./routes/galleryofmywork');
-var getaquoteRouter = require('./routes/getaquote');
-var contactmeRouter = require('./routes/contactme');
-var servicesRouter = require('./routes/services');
-var contactRouter = require('./routes/contact');
-var quoteRouter = require('./routes/quote');
+const app = express();
+const port = process.env.PORT || 3000;
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// Define static assets directory
 app.use(express.static(path.join(__dirname, 'public')));
-//User pages
-app.use('/', indexRouter);
-app.use('/whatido', whatidoRouter);
-app.use('/galleryofmywork', galleryofmyworkRouter);
-app.use('/getaquote', getaquoteRouter);
-app.use('/contactme', contactmeRouter);
-//Service calls to db
 
-//get a list of services provided
-app.use('/services', servicesRouter);
-//my a contact request
-app.use('/contact', contactRouter);
-//get a quote for a project
-app.use('/quote', quoteRouter);
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Define routes
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get('/what-i-do', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'what-i-do.html'));
 });
 
-module.exports = app;
+app.get('/gallery', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'gallery.html'));
+});
+
+app.get('/request-quote', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'request-quote.html'));
+});
+
+app.get('/contact-me', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'contact-me.html'));
+});
+
+// Start the server
+app.listen(port, () => console.log(`Server listening on port ${port}`));
